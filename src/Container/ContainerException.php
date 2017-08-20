8394c25a54236edc76708b41d2ea2c95217084c1
Emre Sokullu <esokullu@gmail.com>
2017-08-20 02:41:50 -0700

    Create ContainerException.php

---
+++ src/Container/ContainerException.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * @author Klyachin Andrey <akliachin@kiwitaxi.com>
+ */
+
+namespace CapMousse\ReactRestify\Errors\Container;
+
+use Psr\Container\ContainerExceptionInterface;
+
+class ContainerException extends \Exception implements ContainerExceptionInterface
+{
+}
